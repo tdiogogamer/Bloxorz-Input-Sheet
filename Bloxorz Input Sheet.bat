b7fc778f741232cd197fc1d46ea72c4508c8eb06
Diogo <dioguinhoreidelas01@gmail.com>
2022-05-08 17:57:24 -0300

    Bloxorz Input Sheet

---
+++ Bloxorz Input Sheet.bat
@@ -0,0 +1,452 @@
+:0
+title Bloxorz Input Sheet by tdiogo_gamer
+cls
+@echo off
+echo by tdiogo_gamer
+echo 10 Levels - 1
+echo 20 Levels - 2
+echo 33 Levels - 3
+echo Any 10 Levels - 4
+echo Any 20 Levels - 5
+echo Any 33 Levels - 6
+set /p Category=Select Your Category:
+if %Category% == 1 goto 1
+if %Category% == 2 goto 2
+if %Category% == 3 goto 3
+if %Category% == 4 goto 4
+if %Category% == 5 goto 5
+if %Category% == 6 goto 6
+
+:1
+cls
+echo Level 01 - RightX2, Down, RightX3, Down
+pause
+cls
+echo Level 02 - Up, Right, Down, RightX4, UpX2, Down, Right, Down, RightX2, Up, Right, Up
+pause
+cls
+echo Level 03 - Right, Up, RightX3, Up, Left, Down, Right, UpX2, RightX3, DownX3, Right, Up
+pause
+cls
+echo Level 04 - Up, Left, Up, RightX2, Up, RightX6, Down, Right, DownX5, Right, Up, LeftX6
+echo Down
+pause
+cls
+echo Level 05 - LeftX3, Right, LeftX5, Down, Right, DownX2, RightX4, Down, RightX4, LeftX4
+echo Down, LeftX6
+pause
+cls
+echo Level 06 - RightX3, DownX2, Right, DownX2, Right, Down, Right, Up, LeftX3, UpX2, Left
+echo UpX3, RightX3, DownX2, Left, Up, RightX2, Down, Right, DownX2, Right
+pause
+cls
+echo Level 07 - Down, Left, Up, RightX5, Down, Right, Left, Up, LeftX5, Down, Right, Down
+echo Right, Down, RightX3, UpX2, Right, Down, Left, Up, Right, UpX2, RightX3
+echo Down, Right, Down, Right, Down, Left, Up
+pause
+cls
+echo Level 08 - RightX2, Down, Space, UpX4, Down, RightX2
+pause
+cls
+echo Level 09 - Right, Down, RightX6, Up, Right, Left, Down, LeftX4, Up, Space, Right, Down
+echo RightX4, Down
+pause
+cls
+echo Level 10 - RightX2, Left, DownX3, Right, DownX5, LeftX4, Up, LeftX3, Down, Up, RightX3
+echo Down, RightX3, Up, Space, DownX2, RightX2, Down, Right, DownX3, Left, Down
+echo Up, Right, UpX4, LeftX2, Up, Left, Down, LeftX5
+pause
+goto 0
+
+:2
+cls
+echo Level 01 - RightX2, Down, RightX3, Down
+pause
+cls
+echo Level 02 - Up, Right, Down, RightX4, UpX2, Down, Right, Down, RightX2, Up, Right, Up
+pause
+cls
+echo Level 03 - Right, Up, RightX3, Up, Left, Down, Right, UpX2, RightX3, DownX3, Right, Up
+pause
+cls
+echo Level 04 - Up, Left, Up, RightX2, Up, RightX6, Down, Right, DownX5, Right, Up, LeftX6
+echo Down
+pause
+cls
+echo Level 05 - LeftX3, Right, LeftX5, Down, Right, DownX2, RightX4, Down, RightX4, LeftX4
+echo Down, LeftX6
+pause
+cls
+echo Level 06 - RightX3, DownX2, Right, DownX2, Right, Down, Right, Up, LeftX3, UpX2, Left
+echo UpX3, RightX3, DownX2, Left, Up, RightX2, Down, Right, DownX2, Right
+pause
+cls
+echo Level 07 - Down, Left, Up, RightX5, Down, Right, Left, Up, LeftX5, Down, Right, Down
+echo Right, Down, RightX3, UpX2, Right, Down, Left, Up, Right, UpX2, RightX3
+echo Down, Right, Down, Right, Down, Left, Up
+pause
+cls
+echo Level 08 - RightX2, Down, Space, UpX4, Down, RightX2
+pause
+cls
+echo Level 09 - Right, Down, RightX6, Up, Right, Left, Down, LeftX4, Up, Space, Right, Down
+echo RightX4, Down
+pause
+cls
+echo Level 10 - RightX2, Left, DownX3, Right, DownX5, LeftX4, Up, LeftX3, Down, Up, RightX3
+echo Down, RightX3, Up, Space, DownX2, RightX2, Down, Right, DownX3, Left, Down
+echo Up, Right, UpX4, LeftX2, Up, Left, Down, LeftX5
+pause
+cls
+echo Level 11 - RightX4, Up, Left, DownX3, RightX3, Down, Left, Up, LeftX3, UpX2 Right, Up,
+echo RightX2, Down, Right, Up, LeftX3, DownX2, LeftX3, UpX2, Right, UpX2, Left
+echo Down, Right, Up, Right, Down, Left
+pause
+cls
+echo Level 12 - Left, Down, Right, Up, Right, Up, Right, Up, Right, Up, Right, Down, Left, Up
+echo RightX3, DownX3, Left, Up, Right, UpX3, LeftX3, RightX3, DownX3, Left, Down,
+echo Right, Up, Right, Down, Left, UpX3, RightX2 Up, Down, LeftX2, DownX3, Right,
+echo Up, Left, UpX2, LeftX3, DownX2, Left
+pause
+cls
+echo Level 13 - Up, Left, Down, Right, Up, Left, Down, RightX2, Up, Left, UpX2, LeftX7, DownX3
+echo Right, Up, Left, Down, Right, DownX2, Right, DownX2, Right, Up, RightX5, Up
+echo Left, Down, Left, UpX2
+pause
+cls
+echo Level 14 - RightX4, Up, LeftX2, Down, RightX3, DownX4, LeftX3, Down, Rightx2, Down
+echo RightX2, LeftX2, Up, Left, Up, RightX2, UpX2, DownX2, LeftX2, Down, Left, Up
+echo RightX3, UpX4, LeftX6, Up, Right, Down, LeftX5, DownX3, Right, Down, Right
+pause
+cls
+echo Level 15 - RightX4, UpX2, Space, UpX5, RightX3, UpX2, RightX4, DownX2, Space, LeftX4
+echo Space, Up, Down, Up, Left, Up, LeftX3, Down, Space, LeftX5, Down, LeftX2
+echo DownX3, Left, Down, Right, Up, RightX7
+pause
+cls
+echo Level 16 - RightX4, Space, Right, Down, Space, Right, Up, Space, Right, LeftX3, RightX4
+echo LeftX4, RightX2, Space, Down, RightX7
+pause
+cls
+echo Level 17 - DownX5, Left, Up, RightX5, Up, RightX3, LeftX3, Down, LeftX5, UpX4, RightX5
+echo Down, RightX4, Down, Left, Up Right, Down, Up, Right, Down, Left, UpX2, Right
+echo Down, LeftX2, Up, Right, Down, LeftX4, Up, LeftX5, DownX4, RightX5, Up
+echo RightX3 DownX2, Up, Left, Up, LeftX3, Down, LeftX3, Up, Left, UpX3, RightX5
+echo Down, RightX3
+pause
+cls
+echo Level 18 - Down, Left, Up, RightX4, UpX2, DownX2, LeftX3, Up, Right, Down, LeftX2, Up, Right, Down, RightX3, DownX2,
+echo Up2X, LeftX3, Up, Left, Down, Right, Up, LeftX2, DownX4, RightX2, LeftX2, Up4X, Right2X, Down, Left, Up, Right,
+echo Down2X, Left, Up, Right, Down, Right, Up, RightX5, DownX3, Left, Up, Right, DownX2, Left, Up, Right, Down, Left, Up, Right
+pause
+cls
+echo Level 19 - RightX8, Down, Right, Up, LeftX6, Down, Right, Up, RightX5, DownX5, LeftX5,
+echo Down, Left, Up, RightX6, Up, Left, Down, LeftX5, DownX4, RightX3, LeftX6, UpX2
+pause
+cls
+echo Level 20 - Down, Left, Down, Right, Down, Left, UpX2, Right, UpX2, LeftX4, DownX2, Right
+echo Up, LeftX2, DownX3, Left, Down, Up, Right, UpX2, Right, UpX2, RightX3, DownX3
+echo Left, Up, LeftX4, DownX5, Space, Up, Left, Right, Down, Space, RightX4, Down
+pause
+goto 0
+
+:3
+cls
+echo Level 01 - RightX2, Down, RightX3, Down
+pause
+cls
+echo Level 02 - Up, Right, Down, RightX4, UpX2, Down, Right, Down, RightX2, Up, Right, Up
+pause
+cls
+echo Level 03 - Right, Up, RightX3, Up, Left, Down, Right, UpX2, RightX3, DownX3, Right, Up
+pause
+cls
+echo Level 04 - Up, Left, Up, RightX2, Up, RightX6, Down, Right, DownX5, Right, Up, LeftX6
+echo Down
+pause
+cls
+echo Level 05 - LeftX3, Right, LeftX5, Down, Right, DownX2, RightX4, Down, RightX4, LeftX4
+echo Down, LeftX6
+pause
+cls
+echo Level 06 - RightX3, DownX2, Right, DownX2, Right, Down, Right, Up, LeftX3, UpX2, Left
+echo UpX3, RightX3, DownX2, Left, Up, RightX2, Down, Right, DownX2, Right
+pause
+cls
+echo Level 07 - Down, Left, Up, RightX5, Down, Right, Left, Up, LeftX5, Down, Right, Down
+echo Right, Down, RightX3, UpX2, Right, Down, Left, Up, Right, UpX2, RightX3
+echo Down, Right, Down, Right, Down, Left, Up
+pause
+cls
+echo Level 08 - RightX2, Down, Space, UpX4, Down, RightX2
+pause
+cls
+echo Level 09 - Right, Down, RightX6, Up, Right, Left, Down, LeftX4, Up, Space, Right, Down
+echo RightX4, Down
+pause
+cls
+echo Level 10 - RightX2, Left, DownX3, Right, DownX5, LeftX4, Up, LeftX3, Down, Up, RightX3
+echo Down, RightX3, Up, Space, DownX2, RightX2, Down, Right, DownX3, Left, Down
+echo Up, Right, UpX4, LeftX2, Up, Left, Down, LeftX5
+pause
+cls
+echo Level 11 - RightX4, Up, Left, DownX3, RightX3, Down, Left, Up, LeftX3, UpX2 Right, Up,
+echo RightX2, Down, Right, Up, LeftX3, DownX2, LeftX3, UpX2, Right, UpX2, Left
+echo Down, Right, Up, Right, Down, Left
+pause
+cls
+echo Level 12 - Left, Down, Right, Up, Right, Up, Right, Up, Right, Up, Right, Down, Left, Up
+echo RightX3, DownX3, Left, Up, Right, UpX3, LeftX3, RightX3, DownX3, Left, Down,
+echo Right, Up, Right, Down, Left, UpX3, RightX2 Up, Down, LeftX2, DownX3, Right,
+echo Up, Left, UpX2, LeftX3, DownX2, Left
+pause
+cls
+echo Level 13 - Up, Left, Down, Right, Up, Left, Down, RightX2, Up, Left, UpX2, LeftX7, DownX3
+echo Right, Up, Left, Down, Right, DownX2, Right, DownX2, Right, Up, RightX5, Up
+echo Left, Down, Left, UpX2
+pause
+cls
+echo Level 14 - RightX4, Up, LeftX2, Down, RightX3, DownX4, LeftX3, Down, Rightx2, Down
+echo RightX2, LeftX2, Up, Left, Up, RightX2, UpX2, DownX2, LeftX2, Down, Left, Up
+echo RightX3, UpX4, LeftX6, Up, Right, Down, LeftX5, DownX3, Right, Down, Right
+pause
+cls
+echo Level 15 - RightX4, UpX2, Space, UpX5, RightX3, UpX2, RightX4, DownX2, Space, LeftX4
+echo Space, Up, Down, Up, Left, Up, LeftX3, Down, Space, LeftX5, Down, LeftX2
+echo DownX3, Left, Down, Right, Up, RightX7
+pause
+cls
+echo Level 16 - RightX4, Space, Right, Down, Space, Right, Up, Space, Right, LeftX3, RightX4
+echo LeftX4, RightX2, Space, Down, RightX7
+pause
+cls
+echo Level 17 - DownX5, Left, Up, RightX5, Up, RightX3, LeftX3, Down, LeftX5, UpX4, RightX5
+echo Down, RightX4, Down, Left, Up Right, Down, Up, Right, Down, Left, UpX2, Right
+echo Down, LeftX2, Up, Right, Down, LeftX4, Up, LeftX5, DownX4, RightX5, Up
+echo RightX3 DownX2, Up, Left, Up, LeftX3, Down, LeftX3, Up, Left, UpX3, RightX5
+echo Down, RightX3
+pause
+cls
+echo Level 18 - Down, Left, Up, RightX4, UpX2, DownX2, LeftX3, Up, Right, Down, LeftX2, Up, Right, Down, RightX3, DownX2,
+echo Up2X, LeftX3, Up, Left, Down, Right, Up, LeftX2, DownX4, RightX2, LeftX2, Up4X, Right2X, Down, Left, Up, Right,
+echo Down2X, Left, Up, Right, Down, Right, Up, RightX5, DownX3, Left, Up, Right, DownX2, Left, Up, Right, Down, Left, Up, Right
+pause
+cls
+echo Level 19 - RightX8, Down, Right, Up, LeftX6, Down, Right, Up, RightX5, DownX5, LeftX5,
+echo Down, Left, Up, RightX6, Up, Left, Down, LeftX5, DownX4, RightX3, LeftX6, UpX2
+pause
+cls
+echo Level 20 - Down, Left, Down, Right, Down, Left, UpX2, Right, UpX2, LeftX4, DownX2, Right
+echo Up, LeftX2, DownX3, Left, Down, Up, Right, UpX2, Right, UpX2, RightX3, DownX3
+echo Left, Up, LeftX4, DownX5, Space, Up, Left, Right, Down, Space, RightX4, Down
+pause
+cls
+echo Level 21 - Right, Down, Left, Up, Left, Down, Right, Up, RightX2, Up, RightX3, Up, Left
+echo DownX3, UpX3, Right, Down, LeftX3, Down, LeftX2, Down, Left, Up, Right, Down
+echo Right, Up, Left, Down, Right, Up, Left, Down, Right, DownX2, Right, DownX2
+echo RightX3, UpX2, DownX2, LeftX2, Up, Left, Down, RightX3, UpX4, RightX3
+pause
+cls
+echo Level 22 - Right, Up, RightX4, Down, Right, DownX3, Left, Down, Up, Right, UpX3,
+echo Left, Up, Right, Down, Left, Up, Left, Up, LeftX3, Down, Left, Down, Left, 
+echo UpX2, Right, Down, Left, Up, Right, Down, Left, DownX3, Right, Down, Up, 
+echo Left, UpX3, Right, Up, Left, Down, Right, Up, Right, Up, RightX3, Down, RightX4, Up
+pause
+cls
+echo Level 23 - Right, DownX2, Right, Up, RightX5, Up, Left, Down, Right, LeftX3, UpX5, RightX4
+echo Up, Down, LeftX6, DownX4, LeftX7, UpX3, RightX2, Up, Down, Left, Up, Right
+echo DownX2, Right, DownX3, RightX2, Down, Right, UpX6, Right, Down, Left, Up
+pause
+cls
+echo Level 24 - Down, Left, Down, Right, Down, Left, UpX2, DownX2, Right, Up, Left, Up, Right
+echo Down, RightX2, UpX2, Right, Up, Left, Down, Right, Up, RightX4, LeftX5, Down
+echo Right, Left, Up, Right, Down, Left, Up, RightX6, Right, Down, Left, Right, Up
+echo RightX3
+pause
+cls
+echo Level 25 - Up, Right, Down, Left, Up, Right, Down, Left, Up, RightX4, Up, Left, Up, LeftX2
+echo Up, Right, Left, Down, RightX2, DownX2, LeftX4, Down, RightX2, Up, RightX3
+echo UpX2, RightX3, Up, Left, Down, Right, UpX2, Left, Down, Right, Up, Right, Down
+echo Left
+pause
+cls
+echo Level 26 - UpX2, LeftX3, Down, LeftX3, Down, Right, Up, RightX2, UpX2, RightX2, Down
+echo RightX2, Down, RightX2, UpX2, LeftX2, Up, LeftX6, Down, LeftX3, DownX3,
+echo Space, UpX3, LeftX6, Down, LeftX3, DownX3, UpX2, Right, Up, Left, Down, Right,
+echo Up, RightX2, UpX2, RightX2, Down, RightX2, Down, RightX2, UpX2, LeftX2, Up,
+echo LeftX4, DownX5, RightX3, Space, UpX3, LeftX4, DownX5, RightX2, Left
+pause
+cls
+echo Level 27 - RightX5, Up, Left, Down, Right, Up, Right, Down, LeftX6, Down, Right, Up, LeftX2
+echo Down, Right, Up, RightX4, Up, RightX4, DownX3, Left, DownX3, LeftX3, Down,
+echo Left, UpX4, Right, Down, LeftX7, Up, Left, DownX2, Right, Up, Left, Down, Right,
+echo Up, Left
+pause
+cls
+echo Level 28 - Left, DownX3, Right, Down, Left, Up, Right, DownX2, RightX2, DownX2,
+echo RightX3, UpX3, Left, Up, Left, Up, Left, Up, Left, Down, RightX2, Down, Right,
+echo Down, Right, Down, RightX2, Down, Left, Down, LeftX2, UpX3, Left, Up, Left, Up,
+echo LeftX2, Up, Left2X, Down, Left, Up, RightX3, Down, Right, Down, Right, Down,
+echo Right, Down, Right, LeftX4, Space, LeftX6, UpX2, LeftX4, Down, Space, DownX3, LeftX4, UpX2, LeftX4, Up
+pause
+cls
+echo Level 29 - Left, Up, Right, Down, Left, Up, Right, Down, Right, DownX2, RightX2, LeftX2
+echo UpX2, Left, Up, Left, Down, RightX2, Up, Left, Down, Left, UpX2, LeftX2, RightX2
+echo DownX2, Right, Up, Right, Down, Right, Up, Left, Down, Right, UpX2, RightX2
+echo LeftX2, DownX6, RightX2, LeftX2, UpX4, LeftX6, RightX4, Down, Left, Up, RightX6
+echo LeftX5, Up, Left, DownX4, LeftX3, Down, Right, Up, Left, Down, Right, Up, Left
+pause
+cls
+echo Level 30 - Down, Right, DownX2, RightX2, Up, RightX2, Down, RightX4, Up, Down, LeftX4,
+echo Up, LeftX3, DownX2, Left, Up, RightX6, UpX3, RightX3, Up, Right, DownX3,
+echo LeftX2, Down, Left, Down, Right, UpX2, Right, Down, Left, Down, LeftX4, Up,
+echo LeftX2, Down, LeftX2, Up, Left, Down, Left, Up, Right, Up, Down, Left, Down,
+echo Right, Up, Right, Down, RightX2, Up, Left, DownX2, Left, Up, RightX7, Up, Left,
+echo UpX3, RightX3, Up, Right, Up, LeftX5, Down, Left
+pause
+cls
+echo Level 31 - Up, Left, Down, LeftX2, Upx4, DownX4, RightX3, Up, Left, Down, Right, UpX4, Left
+echo Up, Right, DownX2, Left, Up, Right, Down, LeftX3, DownX3, Left, DownX2, UpX2
+echo Right, UpX3, LeftX4, Down, Right, Up, Left, DownX5, Left, Up, Right, Left, Up
+echo Right, Down, Right, Up, RightX2, Up, RightX2, Down, RightX3, Up, Left, Down
+echo Right, UpX5, Right, Down, Left
+pause
+cls
+echo Level 32 - UpX2, Left, Down, Right, Up, Right, Up, Right, Down, Left, Right, Up, Left, Down
+echo Left, Down, Left, Up, Right, Down, Left, Up, Right, Up, Right, Up, Right, Down
+echo LeftX2, DownX4, LeftX4, Up, Right, Down, RightX3, UpX5, Right, Up, RightX2, Up
+echo Down, LeftX2, Down, Left, DownX5, LeftX4, Up, Right, Left, Down, RightX4, UpX5
+echo Right, Up, RightX2, Up, Down, LeftX2, Down, Left, DownX5, LeftX3, Up, Right
+echo Down, RightX2, UpX3, Right, Up, RightX2, Up, Left, Down, Left, Down, Left, Down
+echo Right, Up, LeftX2, Up, LeftX5, Down
+pause
+cls
+echo Level 33 - RightX4, Up, Left, DownX2, Left, Up, Right, UpX2, RightX2, Down, Right, Down
+echo Right, Up, LeftX2, Up, Right, Down, Right, DownX2, Right, Down, Left, DownX2
+echo RightX2, Up, Down, LeftX2, UpX2, Right, Up, Left, UpX2, Left, Up, Left, Down
+echo Right, Up, LeftX3, DownX3, Left, Down, LeftX2, Down, Left, Up
+pause
+goto 0
+
+:4
+cls
+echo Level 01 - RightX2, Down, RightX3, Down
+pause
+cls
+echo Level 02 - Up, Right, Down, RightX4, UpX2, Down, Right, Down, RightX2, Up, Right, Up
+pause
+cls
+echo Level 03 - Right, Up, RightX3, Up, Left, Down, Right, UpX2, RightX3, DownX3, Right, Up
+pause
+cls
+echo Level 04 - Up, Left, Up, RightX2, Up, RightX6, Down, Right, DownX5, Right, Up, LeftX6
+echo Down
+pause
+cls
+echo Level 05 - LeftX3, Right, LeftX5, Down, Right, DownX2, RightX4, Down, RightX4, LeftX4
+echo Down, LeftX6
+pause
+cls
+echo Level 06 - RightX3, DownX2, Right, DownX2, Right, Down, Right, Up, LeftX3, UpX2, Left
+echo UpX3, RightX3, DownX2, Left, Up, RightX2, Down, Right, DownX2, Right
+pause
+cls
+echo Level 07 - Down, Left, Up, RightX5, Down, Right, Left, Up, LeftX5, Down, Right, Down
+echo Right, Down, RightX3, UpX2, Right, Down, Left, Up, Right, UpX2, RightX3
+echo Down, Right, Down, Right, Down, Left, Up
+pause
+cls
+echo Level 8 - RightX2, Right, Space, Right, Space, Right, Space, Right, Respawn,
+echo RightX2, Down, Space, UpX4, Down, RightX2
+pause
+cls
+echo Level 10 - RightX2, Left, DownX3, Right, DownX5, LeftX4, Up, LeftX3, Down, Up, RightX3
+echo Down, RightX3, Up, Space, DownX2, RightX2, Down, Right, DownX3, Left, Down
+echo Up, Right, UpX4, LeftX2, Up, Left, Down, LeftX5
+pause
+goto 0
+
+:5
+cls
+echo Level 01 - RightX2, Down, RightX3, Down
+pause
+cls
+echo Level 02 - Up, Right, Down, RightX4, UpX2, Down, Right, Down, RightX2, Up, Right, Up
+pause
+cls
+echo Level 03 - Right, Up, RightX3, Up, Left, Down, Right, UpX2, RightX3, DownX3, Right, Up
+pause
+cls
+echo Level 04 - Up, Left, Up, RightX2, Up, RightX6, Down, Right, DownX5, Right, Up, LeftX6
+echo Down
+pause
+cls
+echo Level 05 - LeftX3, Right, LeftX5, Down, Right, DownX2, RightX4, Down, RightX4, LeftX4
+echo Down, LeftX6
+pause
+cls
+echo Level 06 - RightX3, DownX2, Right, DownX2, Right, Down, Right, Up, LeftX3, UpX2, Left
+echo UpX3, RightX3, DownX2, Left, Up, RightX2, Down, Right, DownX2, Right
+pause
+cls
+echo Level 07 - Down, Left, Up, RightX5, Down, Right, Left, Up, LeftX5, Down, Right, Down
+echo Right, Down, RightX3, UpX2, Right, Down, Left, Up, Right, UpX2, RightX3
+echo Down, Right, Down, Right, Down, Left, Up
+pause
+cls
+echo Level 8 - RightX2, Right, Space, Right, Space, Right, Space, Right, Respawn,
+echo RightX2, Right, Space, Right, Space, Right, Space, Right, Respawn,
+echo RightX2, Right, Space, Right, Space, Right, Space, Right, Respawn,
+echo HOLD Right, Left, Space, Right (Do It Fast), Right, Space, Left, Respawn,
+echo RightX2, Down, Space, UpX4, Down, RightX2
+pause
+cls
+echo Level 20 - Down, Left, Down, Right, Down, Left, UpX2, Right, UpX2, LeftX4, DownX2, Right
+echo Up, LeftX2, DownX3, Left, Down, Up, Right, UpX2, Right, UpX2, RightX3, DownX3
+echo Left, Up, LeftX4, DownX5, Space, Up, Left, Right, Down, Space, RightX4, Down
+pause
+goto 0
+
+:6
+cls
+echo Level 01 - RightX2, Down, RightX3, Down
+pause
+cls
+echo Level 02 - Up, Right, Down, RightX4, UpX2, Down, Right, Down, RightX2, Up, Right, Up
+pause
+cls
+echo Level 03 - Right, Up, RightX3, Up, Left, Down, Right, UpX2, RightX3, DownX3, Right, Up
+pause
+cls
+echo Level 04 - Up, Left, Up, RightX2, Up, RightX6, Down, Right, DownX5, Right, Up, LeftX6
+echo Down
+pause
+cls
+echo Level 05 - LeftX3, Right, LeftX5, Down, Right, DownX2, RightX4, Down, RightX4, LeftX4
+echo Down, LeftX6
+pause
+cls
+echo Level 06 - RightX3, DownX2, Right, DownX2, Right, Down, Right, Up, LeftX3, UpX2, Left
+echo UpX3, RightX3, DownX2, Left, Up, RightX2, Down, Right, DownX2, Right
+pause
+cls
+echo Level 07 - Down, Left, Up, RightX5, Down, Right, Left, Up, LeftX5, Down, Right, Down
+echo Right, Down, RightX3, UpX2, Right, Down, Left, Up, Right, UpX2, RightX3
+echo Down, Right, Down, Right, Down, Left, Up
+pause
+cls
+echo Level 8 - RightX2, Right, Space, Right, Space, Right, Space, Right, Respawn,
+echo RightX2, Right, Space, Right, Space, Right, Space, Right, Respawn,
+echo RightX2, Right, Space, Right, Space, Right, Space, Right, Respawn,
+echo RightX2, Right, Space, Right, Space, Right, Space, Right, Respawn,
+echo RightX2, Right, Space, Right, Space, Right, Space, Right, Respawn,
+echo RightX2, Down, Space, UpX4, Down, RightX2, Quit
+pause
+cls
+echo Level 33 - RightX4, Up, Left, DownX2, Left, Up, Right, UpX2, RightX2, Down, Right, Down
+echo Right, Up, LeftX2, Up, Right, Down, Right, DownX2, Right, Down, Left, DownX2
+echo RightX2, Up, Down, LeftX2, UpX2, Right, Up, Left, UpX2, Left, Up, Left, Down
+echo Right, Up, LeftX3, DownX3, Left, Down, LeftX2, Down, Left, Up
+pause
+cls
+goto 0
